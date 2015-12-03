2e2a29864a762f65e5077dbf5be7884fc8f5615c
zhenchaochen <charlie.chen.v@gmail.com>
2015-12-03 11:29:57 -0800

    add shuffle

--- bokeh_plot/plot.py
+++ bokeh_plot/plot.py
@@ -18,13 +18,32 @@ def plot_size_count(dir,title):
     for i in range(0,length):
         if(data_dir[i] == dir):
             data_to_show.append(data_size[i])
+
     print "Total data:", sum(data_to_show) , "MB"
     
+    
     p = charts.Histogram(data_to_show,bins=100,title=title)
-
     charts.output_file("/tmp/"+title+".html",title=title)
-
     charts.show(p)
+
+def plot_shuffle_size_count(dir,title):
+    data = read_csv()
+    data_size = map(lambda x: x/1024, data.SIZE.tolist())
+    data_dir = data.D.tolist()
+    data_path = data.PATHNAME.tolist()
+    length = len(data_size)
+    data_to_show = []
+    for i in range(0,length):
+        if(data_dir[i] == dir and 'shuffle' in data_path[i]):
+            data_to_show.append(data_size[i])
+
+    print "Total shuffle data:", sum(data_to_show) , "MB"
+    
+    
+    p = charts.Histogram(data_to_show,bins=100,title=title)
+    charts.output_file("/tmp/"+title+".html",title=title)
+    charts.show(p)
+
 
 def plot_size_time(dir,title):
     data = read_csv()
@@ -47,7 +66,6 @@ def plot_size_time(dir,title):
 
     r.line(x, y, color='#1F78B4', legend=title)
     #r.line(dates, choam, color='#FB9A99', legend='CHOAM')
-
     plotting.show(r)  # open a browser
 
 def plot_addressOftime():       # Y: address, X: time
@@ -60,6 +78,12 @@ def plot_seqOftime():           #1: random; 2: seq
     pass
 
 def plot_dependencyOftime():           #1: check dependency
+    pass
+
+def plot_shuffleaddressOftime():       # Y: address, X: time
+    pass
+
+def plot_task_type_pi():    #percentage of shuffle,part-in,part-out
     pass
 
 def plot_read_type_pi():        #percentage of 4K, 16K, 64K IO.
@@ -110,7 +134,10 @@ def plot_write_1024k_time():
 
 if __name__ == '__main__':
     plot_size_count('R','read_size_count')
+    plot_shuffle_size_count('R','read_shuffle_size_count')
     plot_size_count('W','write_size_count')
+    plot_shuffle_size_count('W','write_shuffle_size_count')
+
     plot_size_time('R','read_size_time')
     plot_size_time('W','write_size_time')
     
