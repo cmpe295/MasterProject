cc3a3e71d99e4e230e187a987f14def51ca94cfd
zhenchaochen <charlie.chen.v@gmail.com>
2016-03-07 11:33:19 -0800

    add random pattern

--- io/RunGen.py
+++ io/RunGen.py
@@ -5,8 +5,8 @@ myGen = Gen({
         'range': (1,8*1024*1024*1024//512),
         'count': 2*1024*1024*1024//4096,
         'write_percent': 0.5,
-        'write_ran_percent': 0.7,
-        'read_ran_percent': 0.7
+        'write_ran_percent': 1.0,
+        'read_ran_percent': 1.0
     })
 myGen.gen()
 
