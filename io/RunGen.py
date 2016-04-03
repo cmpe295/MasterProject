d36d11667fc8a74777dc7cd0677b34d8e8a1ec1a
zhenchaochen <charlie.chen.v@gmail.com>
2016-04-02 18:06:06 -0700

    add two parameters: buffer size and io range in genTraining data

--- io/RunGen.py
+++ io/RunGen.py
@@ -2,11 +2,11 @@ from Gen import Gen
 
 #2GB data, 20GB address range
 myGen = Gen({
-        'range': (1,10*1024*1024*1024//512),
+        'range': (1,0.5*1024*1024*1024//512),
         'count': 2*1024*1024*1024//4096,
         'write_percent': 0.5,
-        'write_ran_percent': 0.7,
-        'read_ran_percent': 0.7
+        'write_ran_percent': 0.67,
+        'read_ran_percent': 0.67
     })
 myGen.gen()
 
--- io/TrainingGen.py
+++ io/TrainingGen.py
@@ -1,3 +1,4 @@
+from Gen import Gen
 from Shaper import Shaper
 from Statistics import Statistics
 
@@ -7,35 +8,48 @@ write_seq_threshold = 128       #128KB
 capacity = 400*1000 #400GB
 text_file = open("Training-BufSize.txt", "w")
 
-for i in xrange(1,1200,1):
-    ##############################################################
-    capacity = i*1000        #200MB
-    myShaper = Shaper({
-            'capacity': capacity,        
-            'full_watermark': full_watermark,
-            'hot_watermark': hot_watermark,
-            'write_seq_threshold': write_seq_threshold
-        })
-    myShaper.run('./output/gen.csv','./output/optimize.csv')
-    myShaper.print_io_count()
+for genRange in xrange(512, 16*1024, 64): #from 512GB to 20TB
 
-    ##############################################################
-    #400GB SSD with optimizer
-    myStat = Statistics({
-        'file'          : './output/optimize.csv',
-        'ssd_size'      : 200*1024*1024,        
-        'ssd_seq_write' : 400*1024*1024,
-        'ssd_seq_read'  : 500*1024*1024,
-        'ssd_ran_write' : 80*1000,
-        'ssd_ran_read'  : 100*1000,
-        'ssd_read_hit'   : 0.91,                
-        'hdd_seq_write' : 400*1024*1024,
-        'hdd_seq_read'  : 500*1024*1024,
-        'hdd_ran_write' : 1400,
-        'hdd_ran_read'  : 1400
+    myGen = Gen({
+        'range': (1,genRange*1024*1024//512),
+        'count': 2*1024*1024*1024//4096,
+        'write_percent': 0.5,
+        'write_ran_percent': 0.67,
+        'read_ran_percent': 0.67
     })
-    myStat.run()
-    text_file.write("%d,%f\n" % (i,myStat.total_time))
+    myGen.gen()
+
+
+
+    for bufSize in xrange(1,1024,4):
+        ##############################################################
+        capacity = bufSize*1024        #200MB
+        myShaper = Shaper({
+                'capacity': capacity,        
+                'full_watermark': full_watermark,
+                'hot_watermark': hot_watermark,
+                'write_seq_threshold': write_seq_threshold
+            })
+        myShaper.run('./output/gen.csv','./output/optimize.csv')
+        myShaper.print_io_count()
+
+        ##############################################################
+        #400GB SSD with optimizer
+        myStat = Statistics({
+            'file'          : './output/optimize.csv',
+            'ssd_size'      : 200*1024*1024,        
+            'ssd_seq_write' : 400*1024*1024,
+            'ssd_seq_read'  : 500*1024*1024,
+            'ssd_ran_write' : 80*1000,
+            'ssd_ran_read'  : 100*1000,
+            'ssd_read_hit'   : 0.91,                
+            'hdd_seq_write' : 400*1024*1024,
+            'hdd_seq_read'  : 500*1024*1024,
+            'hdd_ran_write' : 1400,
+            'hdd_ran_read'  : 1400
+        })
+        myStat.run()
+        text_file.write("%d,%d,%f\n" % (genRange,bufSize,myStat.total_time))
 
 text_file.close()
 exit(1)
